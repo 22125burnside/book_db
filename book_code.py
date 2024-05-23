15ef8de28ce34b7d4d240011d16609cdf6cd26c5
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-24 10:25:35 +1200

    Let the user chose data from a genre

--- book_code.py
+++ book_code.py
@@ -91,6 +91,7 @@ def print_all_romantasy():
     JOIN genre ON book.genre_id = genre.genre_id
     JOIN author on book.author_id = author.author_id
     WHERE genre.genre_id = "1"
+    ORDER BY author_name ASC
 """
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -98,6 +99,216 @@ def print_all_romantasy():
     print("Book Name                       Pages     Rating    Type           Genre          Author")
     for book in results:
         print(f"{book[0]:<32}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<15}{book[5]}")
+    db.close
+
+
+# print all romance books
+def print_all_romance():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "2"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
+    db.close
+
+
+# print all fantasy books
+def print_all_fantasy():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "3"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
+    db.close
+
+
+# print all historical fiction books
+def print_all_history():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "4"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
+    db.close
+
+
+# print all thriller books
+def print_all_thriller():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "5"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
+    db.close
+
+
+# print all mystery books
+def print_all_mystery():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "6"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
+    db.close
+
+
+# print all dystiopian books
+def print_all_dystopian():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "7"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
+    db.close
+
+
+# print all contemporary books
+def print_all_contemporary():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "8"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
+    db.close
+
+
+# print all classic books
+def print_all_classic():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "9"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
+    db.close
+
+
+# print all mythology books
+def print_all_mythology():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "10"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
+    db.close
+
+
+# print all horror books
+def print_all_horror():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # Put in my sql statement
+    sql = """SELECT book.name, book.pages, book.rating, book.type, genre.genre_name, author.author_name
+    FROM book
+    JOIN genre ON book.genre_id = genre.genre_id
+    JOIN author on book.author_id = author.author_id
+    WHERE genre.genre_id = "11"
+    ORDER BY author_name ASC
+"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print it nicely
+    print("Book Name                                    Pages     Rating    Type           Genre               Author")
+    for book in results:
+        print(f"{book[0]:<45}{book[1]:<10}{book[2]:<10}{book[3]:<15}{book[4]:<20}{book[5]:<20}")
     db.close
 
 
@@ -122,3 +333,23 @@ What would you like to see?
         user_input = input("What genre would you like to see?\n")
         if user_input == "1":
             print_all_romantasy()
+        elif user_input == "2":
+            print_all_romance()
+        elif user_input == "3":
+            print_all_fantasy()
+        elif user_input == "4":
+            print_all_history()
+        elif user_input == "5":
+            print_all_thriller()
+        elif user_input == "6":
+            print_all_mystery()
+        elif user_input == "7":
+            print_all_dystopian()
+        elif user_input == "8":
+            print_all_contemporary()
+        elif user_input == "9":
+            print_all_classic()
+        elif user_input == "10":
+            print_all_mythology()
+        elif user_input == "11":
+            print_all_horror()
